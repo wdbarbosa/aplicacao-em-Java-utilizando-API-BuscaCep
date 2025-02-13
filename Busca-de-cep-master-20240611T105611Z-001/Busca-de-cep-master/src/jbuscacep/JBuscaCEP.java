45528d683b25c8400dbc8365af48f0eba76d1467
Wendel Rafael Barbosa <wendel.rafael@unesp.br>
2025-02-13 16:52:53 -0300

    programa

---
+++ Busca-de-cep-master-20240611T105611Z-001/Busca-de-cep-master/src/jbuscacep/JBuscaCEP.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jbuscacep;
+
+import frames.JFrmBuscaCep;
+
+/**
+ *
+ * @author Professor
+ */
+public class JBuscaCEP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         JFrmBuscaCep w=new JFrmBuscaCep();
+         w.setVisible(true);
+    }
+    
+}
